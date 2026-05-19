f505674ee40b28064ffec099bb559f1c6192d4f8
Ovidiu <Ovidiuolariu0@gmail.com>
2026-05-19 23:39:10 +0200

    username email password: kleine änderung

--- backend/schemas.py
+++ backend/schemas.py
@@ -4,9 +4,9 @@ from pydantic import BaseModel
 # --- Auth-Schemas ---
 
 class UserRegister(BaseModel):
-    username: str
-    email: str
-    password: str
+    username: str = Field(min_length=3, max_length=50)
+    email:    EmailStr
+    password: str = Field(min_length=8, max_length=128)
 
 
 class UserResponse(BaseModel):
